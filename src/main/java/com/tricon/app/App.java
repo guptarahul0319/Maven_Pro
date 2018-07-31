5d34401b57a3d0ff5dba3be6bf67b3d524c1476f
Rahul Gupta <guptarahul0319@gmail.com>
2018-07-31 18:51:25 +0530

    maven

---
+++ src/main/java/com/tricon/app/App.java
@@ -0,0 +1,23 @@
+package com.tricon.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i=0,j=0;
+		
+		for(i=0;i<5;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print( "*" );	
+			}	
+			
+		System.out.println();	
+		}
+    }
+}
